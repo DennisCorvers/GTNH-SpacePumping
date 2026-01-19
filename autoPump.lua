09b66cde66534c5b194362c3d128d76108565fde
Dennis Corvers <denniscorvers@hotmail.com>
2026-01-19 22:27:36 +0100

    Add various info/debug messages. Prevent AE2 / Config / Card mismatch from crashing the program.

--- autoPump.lua
+++ autoPump.lua
@@ -8,97 +8,118 @@ local r = component.redstone
 local master = {}
 
 -- ===================== FUNCTIONS ======================
+local function mapCards()
+	for i=1, robot.inventorySize() + 1 do
+		local card = inv.getStackInInternalSlot(i)
 
-function mapCards()
-  for i=1, robot.inventorySize() + 1 do
-    local card = inv.getStackInInternalSlot(i)
+		-- Update card slot, add to master
+		if card ~= nil then
+			if config[card.label] then
+				config[card.label].slot = i
+				master[card.label] = config[card.label]
+			else
+				print(string.format("ERROR: Card '%s' in slot %d is not in config.lua!", card.label, i))
+			end
+		else
+			break
+		end
+	end
+end
 
-    -- Update card slot, add to master
-    if card ~= nil then
-      config[card.label].slot = i
-      master[card.label] = config[card.label]
-    else
-      break
+local function updateFluids()
+    print('Updating all fluids...')
+    for _, fluid in pairs(master) do
+        fluid.amount = -1
     end
-  end
-end
 
-
-function updateFluids()
-  for _, fluid in pairs(master) do
-    fluid.amount = 0
-  end
-
-  -- Retrieve from ME network
-  for _, fluid in ipairs(me.getFluidsInNetwork()) do
-    if master[fluid.label] ~= nil then
-      master[fluid.label].amount = fluid.amount
+    for _, fluid in ipairs(me.getFluidsInNetwork()) do
+        if master[fluid.label] ~= nil then
+            master[fluid.label].amount = fluid.amount
+        else
+            print("WARNING: Mismatch: ME has '" .. fluid.label .. "', but no matching Config.")
+        end
     end
-  end
-end
 
-
-function selectFluid()
-  local lowest = nil
-  local lowestPercent = 1
-
-  -- Iterate over the known cards
-  for label, fluid in pairs(master) do
-    local percent = fluid.amount / fluid.target
-    if percent < lowestPercent then
-      lowest = label
-      lowestPercent = percent
+    for label, fluid in pairs(master) do
+        if fluid.amount == -1 then
+            master[label] = nil
+        end
     end
-  end
-  return lowest
 end
 
+local function selectFluid()
+	local lowest = nil
+	local lowestPercent = 1
 
-function calcDuration(fluid)
-  return 1.05 * (master[fluid].target - master[fluid].amount) / (master[fluid].rate * config.mult)
+	-- Iterate over the known cards
+	for label, fluid in pairs(master) do
+		local percent = fluid.amount / fluid.target
+		if percent < lowestPercent then
+			lowest = label
+			lowestPercent = percent
+		end
+	end
+	return lowest
 end
 
+local function calcDuration(fluid)
+    local duration = 1.05 * (master[fluid].target - master[fluid].amount) / (master[fluid].rate * config.mult)
+    return math.min(300, duration)
+end
 
-function setFluid(fluid, duration)
-  -- Use appropriate card
-  robot.select(master[fluid].slot)
-  inv.equip()
-  robot.use()
-  inv.equip()
-  r.setOutput(sides.front, 15)
+local function waitStopPump()
+    local timeout = 2
+    r.setOutput(sides.front, 0)
 
-  -- Wait set duration
-  os.sleep(duration)
+    print(string.format('autoPump: Waiting for %d seconds for pump to stop...', timeout))
+    os.sleep(2)
+end
+
+local function setFluid(fluid, duration)
+	-- Use appropriate card
+	local cardindex = master[fluid].slot
+	local card = inv.getStackInInternalSlot(cardindex)
+	print(string.format('autoPump: Selecting card: %s', card.label))
+  print(string.format('autoPump: Switching to %s for %d seconds', fluid, duration))
+  print()
+	robot.select(cardindex)
+	inv.equip()
+	robot.use()
+	inv.equip()
+  
+	r.setOutput(sides.front, 15)
+
+	-- Wait set duration
+	os.sleep(duration)
 end
 
 -- ======================== MAIN ========================
-
 local function main()
-  print('autoPump: Mapping Cards')
-  mapCards()
+	-- Start by shutting off the pump, so the robot can do its work.
+	waitStopPump()
+	
+	print('autoPump: Mapping Cards')
+	mapCards()
 
-  -- THE LOOP
-  while true do
+	-- THE LOOP
+	while true do
 
-    -- Determine lowest fluid (% of target)
-    updateFluids()
-    fluid = selectFluid()
+		-- Determine lowest fluid (% of target)
+		updateFluids()
+		fluid = selectFluid()
 
-    -- Set pump settings
-    if fluid ~= nil then
-      print(string.format('autoPump: Switching to %s', fluid))
-      duration = calcDuration(fluid)
-      setFluid(fluid, duration)
-    else
-      -- All targets reached, sleep for 3 minutes
-      print('autoPump: Standing by...')
-      os.sleep(180)
-    end
+		-- Set pump settings
+		if fluid ~= nil then
+			duration = calcDuration(fluid)
+			setFluid(fluid, duration)
+		else
+			-- All targets reached, sleep for 3 minutes
+			print('autoPump: Standing by...')
+			os.sleep(180)
+		end
 
-    -- Pause to change fluid
-    r.setOutput(sides.front, 0)
-    os.sleep(1)
-
+		-- Pause to change fluid
+		waitStopPump()
   end
 end
 
